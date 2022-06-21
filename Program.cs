eb7f4c899e654acae17dd60cff90541c732ec154
Karl Mellinger <karl.mellinger@microtec.us>
2022-06-21 08:56:21 -0700

    Committing First File

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Side 1 Length");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter Side 2 Length");
+        double y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The Hypotenuse is: " + Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+
+    }
+}
